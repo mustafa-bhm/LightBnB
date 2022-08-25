962f54651e2b5dd9b942d329a3ff934d86bb687e
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-08-25 16:15:50 +0000

    refactoring getUserWithId & addUser

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -31,7 +31,7 @@ const getUserWithEmail = function (email) {
       [email]
     )
     .then((result) => {
-      console.log(result.rows[0]);
+      // console.log(result.rows[0]);
       if (result) {
         return result.rows[0];
       } else {
@@ -41,18 +41,6 @@ const getUserWithEmail = function (email) {
     .catch((err) => console.error("query error", err.stack));
 };
 
-// const getUserWithEmail = function (email) {
-//   let user;
-//   for (const userId in users) {
-//     user = users[userId];
-//     if (user.email.toLowerCase() === email.toLowerCase()) {
-//       break;
-//     } else {
-//       user = null;
-//     }
-//   }
-//   return Promise.resolve(user);
-// };
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -61,8 +49,27 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(
+      `
+  SELECT *
+  FROM users
+  WHERE id = $1
+  `,
+      [id]
+    )
+    .then((result) => {
+      // console.log("++++", result.rows[0]);
+      if (result) {
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+
+    .catch((err) => console.error("query error", err.stack));
 };
+
 exports.getUserWithId = getUserWithId;
 
 /**
@@ -71,11 +78,23 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(
+      `
+INSERT INTO users(name, email, password)
+VALUES($1, $2, $3)
+RETURNING *;
+`,
+      [user.name, user.email, user.password]
+    )
+    .then((result) => {
+      // console.log("++----", result.rows);
+      return result.rows;
+    })
+
+    .catch((err) => console.error("query error", err.stack));
 };
+
 exports.addUser = addUser;
 
 /// Reservations
